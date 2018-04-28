ca9533125f9241049c2ade0e67e182d2c82c88e6
David Leung <6911277+davidleu@users.noreply.github.com>
2018-04-27 20:05:31 -0700

    Fix directory

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 source /home/ec2-user/.bash_profile
 
-cd /home/ec2-user/
+cd /home/ec2-user/legacy-api
 pm2 start --name "RestServer" npm -- run start:rest-server --
 pm2 start --name "SocketServer" npm -- run start:socket-server --
 pm2 start --name "CodeServer" npm -- run start:services/coderunner-service --
